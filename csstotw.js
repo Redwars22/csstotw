9ed68900f846de38396a2420ea5eaa44d7857694
André Pereira <andrewpereira.megabyte@gmail.com>
2022-12-27 10:02:15 -0300

    adicionando mais keywords do CSS

--- csstotw.js
+++ csstotw.js
@@ -11,34 +11,85 @@ var units = {
     px: "px",
     rem: "rem"
 };
+var justify = {
+    "flex-start": "start",
+    "flex-end": "end",
+    "center": "center",
+    "space-between": "between",
+    "space-around": "around",
+    "space-evenly": "evenly"
+};
 var CSSTOTailwindMap = {
+    "display: flex": "flex",
     "margin-top": "mt-",
-    "width: 100%": "w-full"
+    "margin-bottom": "mb-",
+    "margin-left": "ml-",
+    "margin-right": "mr-",
+    "margin": "m-",
+    "padding-top": "pt-",
+    "padding-bottom": "pb-",
+    "padding-left": "pl-",
+    "padding-right": "pr-",
+    "padding": "p-",
+    "width: 100%": "w-full",
+    "font-size": "text-",
+    "font-weight": "font-",
+    "justify-content": "justify-"
 };
 function parseTokens(tokens) {
     var tokensArr = tokens;
     for (var token = 0; token < tokensArr.length; token++) {
-        if (CSSTOTailwindMap[tokensArr[token]]) {
-            tokensArr[token] = CSSTOTailwindMap[tokensArr[token]];
+        if (CSSTOTailwindMap[tokensArr[token].trim()]) {
+            tokensArr[token] = CSSTOTailwindMap[tokensArr[token].trim()];
+            continue;
         }
-        if (tokensArr[token].includes(units.px)) {
+        if (tokensArr[token].trim().includes(units.px)) {
             var utils = new Tools();
             tokensArr[token] = tokensArr[token].replace(units.px, "");
             tokensArr[token] = "[".concat(utils.transformToRem(Number(tokensArr[token]))).concat(units.rem, "]");
+            continue;
+        }
+        if (tokensArr[token].trim() == "justify-content") {
+            tokensArr[token] = CSSTOTailwindMap["justify-content"];
+            tokensArr[token + 1] = justify[tokensArr[token + 1].trim()];
+            break;
+        }
+        if (!CSSTOTailwindMap[tokensArr[token].trim()]) {
+            if (Number(tokensArr[token]))
+                tokensArr[token] = "[".concat(tokensArr[token].trim(), "]");
+            continue;
+            //if(isNaN(Number(tokensArr[token]))) tokensArr[token] = `"${tokensArr[token]}"`;
         }
     }
-    return tokensArr.join("");
+    return tokensArr.join("").replace(' ', '');
 }
 while (true) {
     var command = reader("CSStoTW: ");
-    if (command === null || command === void 0 ? void 0 : command.trim().match(/margin-top: .*/)) {
-        var statement = command.split(':');
-        console.log(parseTokens(statement));
+    if (command.includes(';')) {
+        var statements = command.split(';');
+        var parsedTW = void 0;
+        parsedTW = "\"";
+        for (var statement = 0; statement < statements.length; statement++) {
+            if (statements[statement].match(/.*[A-Za-z\-]: .*[0-9A-Za-z\-](px)?/)) {
+                parsedTW += parseTokens(statements[statement].split(":")) + (statement < statements.length - 1 ? " " : "");
+                continue;
+            }
+            for (var CSSRule_1 in CSSTOTailwindMap)
+                if (statements[statement].trim() == CSSRule_1)
+                    parsedTW += CSSTOTailwindMap[CSSRule_1] + (statement < statements.length - 1 ? " " : "");
+        }
+        parsedTW += "\"";
+        console.log("class=" + parsedTW);
+        console.log("className=" + parsedTW);
+        continue;
     }
-    if (command.trim() == "exit")
-        break;
-    for (var CSSRule_1 in CSSTOTailwindMap) {
-        if ((command === null || command === void 0 ? void 0 : command.trim()) == CSSRule_1)
-            console.log(CSSTOTailwindMap[CSSRule_1]);
-    }
+    // if(command?.trim().match(/margin-top: .*/)){
+    //     const statement = command.split(':');
+    //     console.log(parseTokens(statement));
+    // }
+    // if(command.trim() == "exit")
+    //     break;
+    // for(const CSSRule in CSSTOTailwindMap)
+    //     if(command?.trim() == CSSRule)
+    //         console.log(CSSTOTailwindMap[CSSRule])
 }
--- csstotw.ts
+++ csstotw.ts
@@ -11,29 +11,64 @@ const units = {
     rem: "rem"
 }
 
+const justify = {
+    "flex-start": "start",
+    "flex-end": "end",
+    "center": "center",
+    "space-between": "between",
+    "space-around": "around",
+    "space-evenly": "evenly",
+}
+
 const CSSTOTailwindMap = {
     "display: flex": "flex",
     "margin-top": "mt-",
+    "margin-bottom": "mb-",
+    "margin-left": "ml-",
+    "margin-right": "mr-",
+    "margin": "m-",
+    "padding-top": "pt-",
+    "padding-bottom": "pb-",
+    "padding-left": "pl-",
+    "padding-right": "pr-",
+    "padding": "p-",
     "width: 100%": "w-full",
+    "font-size": "text-",
+    "font-weight": "font-",
+    "justify-content": "justify-",
 }
 
 function parseTokens(tokens: string[]): string {
     let tokensArr: string[] = tokens;
 
     for(let token = 0; token < tokensArr.length; token++){
-        if(CSSTOTailwindMap[tokensArr[token]]){
-            tokensArr[token] = CSSTOTailwindMap[tokensArr[token]];
+        if(CSSTOTailwindMap[tokensArr[token].trim()]){
+            tokensArr[token] = CSSTOTailwindMap[tokensArr[token].trim()];
+            continue;
         }
 
-        if(tokensArr[token].includes(units.px)){
+        if(tokensArr[token].trim().includes(units.px)){
             const utils = new Tools();
 
             tokensArr[token] = tokensArr[token].replace(units.px, "");
             tokensArr[token] = `[${utils.transformToRem(Number(tokensArr[token]))}${units.rem}]`;
+            continue;
+        }
+
+        if(tokensArr[token].trim() == "justify-content"){
+            tokensArr[token] = CSSTOTailwindMap["justify-content"];
+            tokensArr[token + 1] = justify[tokensArr[token + 1].trim()];
+            break;
+        }
+        
+        if(!CSSTOTailwindMap[tokensArr[token].trim()]) {
+            if(Number(tokensArr[token])) tokensArr[token] = `[${tokensArr[token].trim()}]`;
+            continue;
+            //if(isNaN(Number(tokensArr[token]))) tokensArr[token] = `"${tokensArr[token]}"`;
         }
     }
 
-    return tokensArr.join("");
+    return tokensArr.join("").replace(' ', '');
 }
 
 while(true){
@@ -46,32 +81,32 @@ while(true){
         parsedTW = "\"";
 
         for(let statement = 0; statement < statements.length; statement++){
-            if(statements[statement].match(/margin-top: .*/)){        
-                parsedTW += parseTokens(statements[statement].split(":"));
+            if(statements[statement].match(/.*[A-Za-z\-]: .*[0-9A-Za-z\-](px)?/)){        
+                parsedTW += parseTokens(statements[statement].split(":")) + (statement < statements.length - 1 ? " " : "");
                 continue;
             }
 
             for(const CSSRule in CSSTOTailwindMap)
                 if(statements[statement].trim() == CSSRule)
-                    parsedTW += CSSTOTailwindMap[CSSRule] + " ";
+                    parsedTW += CSSTOTailwindMap[CSSRule] + (statement < statements.length - 1 ? " " : "");
         }  
 
         parsedTW += "\"";
 
-        console.log(parsedTW);
+        console.log("class=" + parsedTW);
+        console.log("className=" + parsedTW);
         continue;
     }
+    // if(command?.trim().match(/margin-top: .*/)){
+    //     const statement = command.split(':');
 
-    if(command?.trim().match(/margin-top: .*/)){
-        const statement = command.split(':');
+    //     console.log(parseTokens(statement));
+    // }
 
-        console.log(parseTokens(statement));
-    }
+    // if(command.trim() == "exit")
+    //     break;
 
-    if(command.trim() == "exit")
-        break;
-
-    for(const CSSRule in CSSTOTailwindMap)
-        if(command?.trim() == CSSRule)
-            console.log(CSSTOTailwindMap[CSSRule])
+    // for(const CSSRule in CSSTOTailwindMap)
+    //     if(command?.trim() == CSSRule)
+    //         console.log(CSSTOTailwindMap[CSSRule])
 }
